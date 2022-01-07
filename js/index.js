a5b694f984a5501dd77a6b95ed6b2f245420c6cb
rouwe <santosroweme14@gmail.com>
2022-01-08 04:16:58 +0800

    Desktop hero section interface

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@
 const headerNav = document.getElementById('header-nav');
 const openMenu = document.getElementById('open-menu');
 const closeMenu = document.getElementById('close-menu');
+const heroSection = document.getElementById('hero-section');
 openMenu.addEventListener("click", openMainMenu);
 closeMenu.addEventListener("click", closeMainMenu);
 
@@ -13,10 +14,12 @@ function openMainMenu() {
     headerNav.style.display = 'block';
     closeMenu.style.display = 'block';
     openMenu.style.display = 'none';
+    heroSection.classList.add('menu-open-bg');
 }
 function closeMainMenu() {
     // Close main menu
     headerNav.style.display = 'none';
     closeMenu.style.display = 'none';
     openMenu.style.display = 'block';
+    heroSection.classList.remove('menu-open-bg');
 }
